9b48c94b5d06f7bd452ec9ccf47d22c1fe04173e
KiganTBS <kigan222@mail.ru>
2021-09-23 00:50:41 +0300

    Stage 1: add auth using email and password

---
+++ app/src/main/java/com/example/schedule/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.schedule;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText editTextEmail, editTextPass;
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPass = findViewById(R.id.editTextPass);
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void checkLogin(View view) {
+        String email = editTextEmail.getText().toString().trim();
+        String pass = editTextPass.getText().toString().trim();
+        if(email.isEmpty() || pass.isEmpty()) return;
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Logged", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(LoginActivity.this, R.string.toast_loginError, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
